b331b23c6c8a937ed19bff3e4fb37217b1927727
Evia-H <evi1020@gmail.com>
2019-08-07 23:04:51 +0300

    added signin and registration

--- server.js
+++ server.js
@@ -1,6 +1,50 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.json());
+
+const database = {
+    users: [
+        {
+            id :0,
+            name : 'Laura',
+            email : 'laura@ferg.com',
+            password: 'chester',
+            entries : 0,
+            joined : new Date()
+        }
+    ]
+}
+
+app.get('/',(req,res)=>{
+    res.json(database.users)
+});
+
+app.post('/signin',(req,res)=>{
+    const {email , password} = req.body;
+    const user = database.users[0];
+    if(email === user.email && password === user.password)
+    res.json('success');
+    else
+    res.status(400).json(`error signing in`)
+})
+
+app.post('/register',(req,res)=>{
+    const {email,name,password} = req.body;
+    database.users.push(
+        {
+            id :100,
+            name : name,
+            email : email,
+            password: password,
+            entries : 0,
+            joined : new Date()
+        }
+    )
+    res.json(database.users[database.users.length -1])
+})
 
 app.listen(3001, ()=>{
     console.log('app is running')
